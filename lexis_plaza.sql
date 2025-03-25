00db40e10336f753a36b43686c159913237f0afb
Nirmal Wadugodapitiya <nirmalwa100@gmail.com>
2025-03-25 02:06:35 -0700

    Add files via upload

---
+++ lexis_plaza.sql
@@ -0,0 +1,221 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 01, 2024 at 07:05 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lexis_plaza`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employer`
+--
+
+CREATE TABLE `employer` (
+  `employer_id` int(11) NOT NULL,
+  `first_name` text NOT NULL,
+  `last_name` text NOT NULL,
+  `nic_number` text NOT NULL,
+  `nic_image` longblob NOT NULL,
+  `employer_image` longblob NOT NULL,
+  `email_address` varchar(65) NOT NULL,
+  `contact_number_1` int(11) NOT NULL,
+  `contact_number_2` int(11) DEFAULT NULL,
+  `address` varchar(65) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `entry_and_exit_reporting`
+--
+
+CREATE TABLE `entry_and_exit_reporting` (
+  `use ID` int(11) NOT NULL,
+  `password` int(11) NOT NULL,
+  `type` text NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_infor`
+--
+
+CREATE TABLE `login_infor` (
+  `User_id` varchar(50) DEFAULT NULL,
+  `Password` varchar(50) DEFAULT NULL,
+  `Date_time` varchar(50) DEFAULT NULL,
+  `login_and_logout` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `login_infor`
+--
+
+INSERT INTO `login_infor` (`User_id`, `Password`, `Date_time`, `login_and_logout`) VALUES
+('100', '200', '06-09-2023 21:07:20', 'Login');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `maintains`
+--
+
+CREATE TABLE `maintains` (
+  `month` int(11) DEFAULT NULL,
+  `water_bill` int(11) DEFAULT NULL,
+  `current_bill` int(11) DEFAULT NULL,
+  `telephone_bill` int(11) DEFAULT NULL,
+  `internet_bill` int(11) DEFAULT NULL,
+  `staff_salary` int(11) DEFAULT NULL,
+  `repair` int(11) DEFAULT NULL,
+  `donations` int(11) DEFAULT NULL,
+  `others` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shop`
+--
+
+CREATE TABLE `shop` (
+  `shop_id` int(11) NOT NULL,
+  `shop_name` text NOT NULL,
+  `category` text NOT NULL,
+  `shop_address` varchar(50) NOT NULL,
+  `monthly_payment` int(11) NOT NULL,
+  `employer_id` int(11) NOT NULL,
+  `date` date DEFAULT NULL,
+  `time` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shopping_mall`
+--
+
+CREATE TABLE `shopping_mall` (
+  `Name` text DEFAULT NULL,
+  `Discription` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `staff_id` int(11) NOT NULL,
+  `first_name` text NOT NULL,
+  `last_name` text NOT NULL,
+  `nic number` text NOT NULL,
+  `contact_number_1` text NOT NULL,
+  `contact_number_2` text NOT NULL,
+  `email_address` text NOT NULL,
+  `category` text NOT NULL,
+  `monthly_salary` int(11) NOT NULL DEFAULT 0,
+  `date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_register`
+--
+
+CREATE TABLE `user_register` (
+  `user_id` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_register`
+--
+
+INSERT INTO `user_register` (`user_id`, `password`) VALUES
+('100', '200');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `employer`
+--
+ALTER TABLE `employer`
+  ADD PRIMARY KEY (`employer_id`);
+
+--
+-- Indexes for table `shop`
+--
+ALTER TABLE `shop`
+  ADD PRIMARY KEY (`shop_id`),
+  ADD KEY `employer_id` (`employer_id`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`staff_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employer`
+--
+ALTER TABLE `employer`
+  MODIFY `employer_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `shop`
+--
+ALTER TABLE `shop`
+  MODIFY `shop_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `staff_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `shop`
+--
+ALTER TABLE `shop`
+  ADD CONSTRAINT `FK_shop_employer` FOREIGN KEY (`employer_id`) REFERENCES `employer` (`employer_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
